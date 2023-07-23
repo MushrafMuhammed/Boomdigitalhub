60101fdd91c9f268b6bc14d6548cd92f25921068
Mushraf Muhammed <mushrafmuhammed@outlook.com>
2023-07-23 17:51:19 +0530

    general categories expanded

--- boom_digital/common/urls.py
+++ boom_digital/common/urls.py
@@ -8,6 +8,17 @@ urlpatterns = [
     # path('', views.null_search_redirect),
     path('home', views.homefun, name='home'),
     path('login', views.loginfun, name='login' ),
-    path('offers', views.offers_hoverfun, name='offers_hover' ),
+    path('offers', views.offersfun, name='offers' ),
+    path('offers_hover', views.offers_hoverfun, name='offers_hover' ),
+    path('mobiles', views.mobilesfun, name='mobile' ),
+    path('laptops', views.laptopfun, name='laptop' ),
+    path('desktops', views.desktopfun, name='desktop' ),
+    path('tablets', views.tabletfun, name='tablet' ),
+    path('accessories', views.accessoriesfun, name='accessories' ),
+    
+
+
+
+
 
 ]
--- boom_digital/common/views.py
+++ boom_digital/common/views.py
@@ -25,3 +25,27 @@ def loginfun(request):
 def offers_hoverfun(request):
     
     return render(request, 'common/offers_hover.html')
+
+def offersfun(request):
+    
+    return render(request, 'common/offers.html')
+
+def mobilesfun(request):
+    
+    return render(request, 'common/mobiles.html')
+
+def laptopfun(request):
+    
+    return render(request, 'common/laptop.html')
+
+def desktopfun(request):
+    
+    return render(request, 'common/desktop.html')
+
+def tabletfun(request):
+    
+    return render(request, 'common/tablets.html')
+
+def accessoriesfun(request):
+    
+    return render(request, 'common/accessories.html')
